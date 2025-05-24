115d958a5ad4c3fe2b13e118b90db27e1fe31ad2
o.moghadas <electromidz@gmail.com>
2025-05-24 17:29:31 +0330

    feat: add backend rust on project

---
+++ apps/backend/target/debug/backend.d
@@ -0,0 +1,1 @@
+/home/o/Documents/Project/Rust/shMessenger/apps/backend/target/debug/backend: /home/o/Documents/Project/Rust/shMessenger/apps/backend/src/main.rs
---
+++ apps/backend/target/debug/deps/backend-fafb0d68589e6eef.d
@@ -0,0 +1,5 @@
+/home/o/Documents/Project/Rust/shMessenger/apps/backend/target/debug/deps/backend-fafb0d68589e6eef: src/main.rs
+
+/home/o/Documents/Project/Rust/shMessenger/apps/backend/target/debug/deps/backend-fafb0d68589e6eef.d: src/main.rs
+
+src/main.rs:
